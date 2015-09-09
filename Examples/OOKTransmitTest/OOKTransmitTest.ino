f2fc4417a6485b53da53536490aa48973bc5845f
kobuki <nls424@gmail.com>
2015-09-09 19:00:28 +0200

    First commit - made reception more robust by tuning RSSI thresholds

---
+++ Examples/OOKTransmitTest/OOKTransmitTest.ino
@@ -0,0 +1,42 @@
+
+#include <RFM69OOK.h>
+#include <SPI.h>
+#include <RFM69registers.h>
+
+RFM69OOK radio;
+unsigned long cnt;
+
+void setup() {
+  Serial.begin(115200);
+  radio.initialize();
+  radio.transmitBegin();
+  radio.setFrequencyMHz(868.88);
+  // radio.setFrequencyMHz(433.99);
+  radio.setPowerLevel(20);
+}
+
+void delayMicros(uint32_t d) {
+  uint32_t t = micros() + d;
+  while(micros() < t);
+}
+
+void loop() {
+  radio.send(1);
+  delayMicros(300L);
+
+  radio.send(0);
+  delayMicros(300L);
+  
+  radio.send(1);
+  delayMicros(200L);
+  
+  radio.send(0);
+  delayMicros(200L);
+
+  radio.send(1);
+  delayMicros(100L);
+  
+  radio.send(0);
+  delayMicros(950 * 1000L);
+}
+
