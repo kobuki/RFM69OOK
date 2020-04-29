b34ddff6919d2dcc9f3ae7f14cab8ca36911aa8f
Marc RIVES <35873376+couin3@users.noreply.github.com>
2020-04-29 12:49:58 +0200

    iot-bus : Bypassed saving SPCR and SPSR on ESP32

--- RFM69OOK.cpp
+++ RFM69OOK.cpp
@@ -265,8 +265,10 @@ void RFM69OOK::writeReg(byte addr, byte value)
 void RFM69OOK::select() {
   noInterrupts();
   // save current SPI settings
+  #ifndef ESP32
   _SPCR = SPCR;
   _SPSR = SPSR;
+  #endif
   // set RFM69 SPI settings
   SPI.setDataMode(SPI_MODE0);
   SPI.setBitOrder(MSBFIRST);
@@ -278,8 +280,10 @@ void RFM69OOK::select() {
 void RFM69OOK::unselect() {
   digitalWrite(_slaveSelectPin, HIGH);
   // restore SPI settings to what they were before talking to RFM69
+  #ifndef ESP32
   SPCR = _SPCR;
   SPSR = _SPSR;
+  #endif
   interrupts();
 }
 
